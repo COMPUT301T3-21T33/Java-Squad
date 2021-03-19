af54f31b8c8665ae6e51dd31d0b69f4d5ee345ff
kyhui111 <62568104+kyhui111@users.noreply.github.com>
2021-03-19 14:29:31 -0600

    User

--- app/src/main/java/com/example/java_squad/MainActivity.java
+++ app/src/main/java/com/example/java_squad/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.java_squad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.java_squad.user.activity.UserLogin;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, UserLogin.class);
+        startActivity(intent);
     }
 }
