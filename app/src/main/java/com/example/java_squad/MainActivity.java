3ce8e43884738ce36658a61059b3e694339e7f07
mikeharbidge <55110294+mikeharbidge@users.noreply.github.com>
2021-03-19 14:57:41 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/java_squad/MainActivity.java
+++ app/src/main/java/com/example/java_squad/MainActivity.java
@@ -1,4 +1,20 @@
 package com.example.java_squad;
 
-public class MainActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.java_squad.user.activity.UserLogin;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, UserLogin.class);
+        startActivity(intent);
+    }
 }
