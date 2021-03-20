4bb3c11f589c4b3fa22df50a9acf6963644b02fb
kyhui111 <62568104+kyhui111@users.noreply.github.com>
2021-03-20 17:53:04 -0600

    Update Main Activity

--- app/src/main/java/com/example/java_squad/MainActivity.java
+++ app/src/main/java/com/example/java_squad/MainActivity.java
@@ -4,14 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvUIDS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvUIDS = (TextView) findViewById(R.id.tv_uids);
+        StringBuilder sb = new StringBuilder();
+
+        TelephonyManager telMan = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        sb.append("Account ID:" + Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID) +"\n");
 
     }
 
