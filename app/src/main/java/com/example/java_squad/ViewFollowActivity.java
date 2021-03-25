443c21694d5d74a5b17c1b4d83a00559ec795261
Frieda0315 <59812863+Frieda0315@users.noreply.github.com>
2021-03-25 13:38:50 -0600

    Update ViewFollowActivity.java

--- app/src/main/java/com/example/java_squad/ViewFollowActivity.java
+++ app/src/main/java/com/example/java_squad/ViewFollowActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.java_squad.user.User;
@@ -19,6 +20,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -45,18 +47,45 @@ public class ViewFollowActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_follow2);
-
+        Intent intent = getIntent();
+        String userid = intent.getStringExtra("id");
         auth = FirebaseAuth.getInstance();
         current = auth.getCurrentUser();
         experimentName =findViewById(R.id.experiment_name);
         addTrail = findViewById(R.id.add_trial_button);
         experimentOwenr = findViewById(R.id.owner);
+        String owner = experimentOwenr.getText().toString();
         database = FirebaseDatabase.getInstance();
         viewQuestion =findViewById(R.id.view_question_button);
         viewStatistics =findViewById(R.id.view_stat_button);
         follow = findViewById(R.id.follow_button);
         String eName = experimentName.getText().toString();
-        myRef = FirebaseDatabase.getInstance().getReference("User").child(current.getUid()).child("follow");
+        myRef = FirebaseDatabase.getInstance().getReference("User");
+        Query query = myRef.orderByChild("name").equalTo(owner);
+        Experimental exp = new Experimental();
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Experimental exp = snapshot.child(owner).getValue(Experimental.class);
+                String description = exp.getDescription();
+                User user = exp.getOwner();
+                String name = exp.getName();
+                String rules = exp.getRules();
+                int type = exp.getType();
+                int minTrials = exp.getMinTrials();
+                exp.setDescription(description);
+                exp.setMinTrials(minTrials);
+                exp.setName(name);
+                exp.setOwner(user);
+                exp.setRules(rules);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
         follow.setOnClickListener(new View.OnClickListener() {
@@ -68,9 +97,8 @@ public class ViewFollowActivity extends AppCompatActivity {
                     follow.setText("following");
                     viewStatistics.setClickable(true);
                     viewQuestion.setClickable(true);
-                    HashMap data = new HashMap();
-                    data.put(eName,eName);
-                    myRef.updateChildren(data);
+
+                    myRef.child("followed").setValue(exp);
 
                 }
                 else{
