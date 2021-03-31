65d3d2b90d43eb0612b9f2259ad0077240ea69c8
Frieda0315 <59812863+Frieda0315@users.noreply.github.com>
2021-03-31 16:56:53 -0600

    Update ExperimentConstructor.java

--- app/src/main/java/com/example/java_squad/ExperimentConstructor.java
+++ app/src/main/java/com/example/java_squad/ExperimentConstructor.java
@@ -1,14 +1,21 @@
 package com.example.java_squad;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 
 import com.example.java_squad.user.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Activity for entering info for a new experiment or updating an old one.
@@ -20,22 +27,62 @@ public class ExperimentConstructor extends AppCompatActivity {
     EditText expRules;
     RadioGroup trialType;
     EditText minTrials;
-
-    User owner;
+    Button submit;
+    FirebaseDatabase db;
+    DatabaseReference df;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_experiment_constructor);
+        Log.d("fried", "you are here");
+        Intent intent = getIntent();
+        String userid = intent.getStringExtra("id");
 
         expName = findViewById(R.id.editText_name);
         expDesc = findViewById(R.id.editText_description);
         expRules = findViewById(R.id.editText_rules);
         minTrials = findViewById(R.id.editText_minTrials);
         trialType = findViewById(R.id.RadioGroup);
+        submit = findViewById(R.id.button_submit);
 
-        Intent intent = getIntent();
-        User owner = (User) intent.getSerializableExtra("user");
+
+        df =  FirebaseDatabase.getInstance().getReference("User").child(userid).child("Experiment");
+
+
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("frieda0", "this is submit");
+
+                int radioButtonID = trialType.getCheckedRadioButtonId();
+                View radioButton = trialType.findViewById(radioButtonID);
+
+                String Ename = expName.getText().toString();
+
+                int Emin = Integer.parseInt(minTrials.getText().toString());
+                int Etype = trialType.indexOfChild(radioButton);
+                String Erule = expRules.getText().toString();
+                String Edescription = expDesc.getText().toString();
+
+
+                Experimental newE = new Experimental(new User(),Ename, Edescription,Erule,Etype, Emin);
+                df.child(Ename).setValue(newE).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+
+                            Log.d("frieda","successful" );
+                        }
+                        else{
+                            Log.d("frieda1", "not successful");
+                        }
+                    }
+                });
+
+            }
+        });
     }
 
     /**
@@ -47,7 +94,7 @@ public class ExperimentConstructor extends AppCompatActivity {
         finish();
     }
 
-    public void submitButton(View view){
+    /*public void submitButton(View view){
         String newName = expName.getText().toString();
         String newDesc = expDesc.getText().toString();
         String newRules = expRules.getText().toString();
@@ -62,5 +109,5 @@ public class ExperimentConstructor extends AppCompatActivity {
         //put exp on firebase
 
         finish();
-    }
+    }*/
 }
